489d1cbbe534994f1e038c01e8ddef148cf21f77
Paweł Mikłas <pablomiklas@gmail.com>
2024-06-27 20:22:51 +0200

    fix: change system and output

--- toggle-sound-source.sh
+++ toggle-sound-source.sh
@@ -11,10 +11,10 @@
 CURRENT_AUDIO_SOURCE=$(SwitchAudioSource -c)
 
 if [ "$CURRENT_AUDIO_SOURCE" == "MacBook Pro Speakers" ]; then
-  SwitchAudioSource -s "USB Audio Device"
+  SwitchAudioSource -t output -s "USB Audio Device" & SwitchAudioSource -t system -s "USB Audio Device"
   CHANGED_AUDIO_SOURCE="USB Audio Device"
 else
-  SwitchAudioSource -s "MacBook Pro Speakers"
+  SwitchAudioSource -t output -s "MacBook Pro Speakers" & SwitchAudioSource -t system -s "MacBook Pro Speakers"
   CHANGED_AUDIO_SOURCE="MacBook Pro Speakers"
 fi
 
